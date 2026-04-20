cf2852ecfab45021cf5b166137af2ee0ff172f91
BT_C09_26 <mobileappdev@bths.edu>
2026-04-20 14:48:33 -0400

    updated text and image

--- GitTest/ContentView.swift
+++ GitTest/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "flag")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Funny and Gitty")
         }
         .padding()
     }
