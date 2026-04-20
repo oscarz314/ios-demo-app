693638c62b232de4f79034d3d6a80cdb977b0fdd
BT_C09_26 <mobileappdev@bths.edu>
2026-04-20 14:44:06 -0400

    Initial Commit

---
+++ GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by Student on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
